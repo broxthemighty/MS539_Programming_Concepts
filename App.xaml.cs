477103354e86f4923566de99e2c8e03acbe85879
ideasarethefuture <121265474+ideasarethefuture@users.noreply.github.com>
2024-05-24 21:03:43 -0500

    Added app.xaml back to project. Accidentally deleted previously.

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WH40K_GUI_UAT_MS539_ML
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the startup window
+            MainWindow wnd = new MainWindow();
+            // Do stuff here, e.g. to the window
+            wnd.Title = "Warhammer 40,000 Roster Editor";
+            // Show the window
+            wnd.Show();
+        }
+    }
+}
