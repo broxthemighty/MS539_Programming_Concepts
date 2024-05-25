a2bf356e906b66d423599b315c38aa20095f4f80
ideasarethefuture <121265474+ideasarethefuture@users.noreply.github.com>
2024-05-24 21:49:47 -0500

    Added additional gui elements.

--- Unit.xaml.cs
+++ Unit.xaml.cs
@@ -19,9 +19,24 @@ namespace WH40K_GUI_UAT_MS539_ML
     /// </summary>
     public partial class Unit : Window
     {
+       
+        
+
         public Unit()
         {
             InitializeComponent();
+            List<string> dummyData = new List<string>();
+            dummyData.Add("Captain");
+            dummyData.Add("Apothecary");
+            dummyData.Add("Intercessors");
+            dummyData.Add("Bladeguard");
+            dummyData.Add("Gladiator Lancer");
+            UnitList.DataContext = dummyData;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
