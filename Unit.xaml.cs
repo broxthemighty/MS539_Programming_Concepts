babbd751a1436275104f2846a10d05ef808d75f3
ideasarethefuture <121265474+ideasarethefuture@users.noreply.github.com>
2024-06-17 20:52:30 -0500

    Added case statements to dummy data for testing. Going to use the same event trigger for the data load from the api in future.

--- Unit.xaml.cs
+++ Unit.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WH40K_GUI_UAT_MS539_ML
 {
@@ -41,17 +43,121 @@ namespace WH40K_GUI_UAT_MS539_ML
             
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void updateUnitWargear(string unitName)
         {
-            //delete this later
+            switch (unitName)
+            {
+                case "StringArrayList":
+                    unitMeleeWeapons.Text = "Melee Weapons Here";
+                    unitRangedWeapons.Text = "Ranged Weapons Here";
+                    unitAbilities.Text = "Abilities Here";
+                    break;
+                case "Captain":
+                    unitMeleeWeapons.Text = "Power Sword";
+                    unitRangedWeapons.Text = "Master Crafted Bolter";
+                    unitAbilities.Text = "Rites of Battle";
+                    break;
+                case "Apothecary":
+                    unitMeleeWeapons.Text = "Melee Weapon";
+                    unitRangedWeapons.Text = "Reductor Pistol";
+                    unitAbilities.Text = "Narthecium";
+                    break;
+                case "Intercessors":
+                    unitMeleeWeapons.Text = "Chainsword and Melee Weapons";
+                    unitRangedWeapons.Text = "Bolt Rifles";
+                    unitAbilities.Text = "Objective Secured";
+                    break;
+                case "Bladeguard":
+                    unitMeleeWeapons.Text = "Master Crafted Power Swords";
+                    unitRangedWeapons.Text = "Heavy Bolt Pistols";
+                    unitAbilities.Text = "Bladeguard";
+                    break;
+                case "Gladiator Lancer":
+                    unitMeleeWeapons.Text = "Armored hull";
+                    unitRangedWeapons.Text = "Lancer laser destroyer";
+                    unitAbilities.Text = "Aquilon Optics";
+                    break;
+            }
+            unitDisplayName.Text = unitName;
+            unitDisplayName.Refresh();
+            unitMeleeWeapons.Refresh();
+            unitRangedWeapons.Refresh();
+            unitAbilities.Refresh();
         }
 
-        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        private void updateUnitStats(string unitName)
         {
-            //put in error checking for unit selection
-
-            //put in switch statement, changing the data in the gui properties based on the selection
-            //this is only for filler dummy data, but will be used later with the api
+            switch(unitName)
+            {
+                case "StringArrayList":
+                    unitMovement.Content = "1";
+                    unitToughness.Content = "1";
+                    unitWounds.Content = "1";
+                    unitSave.Content = "1";
+                    unitLeadership.Content = "1";
+                    unitObjectiveControl.Content = "1";
+                    unitInvulnerableSave.Content = "1";
+                    unitFeelNoPain.Content = "1";
+                    break;
+                case "Captain":
+                    unitMovement.Content = "6";
+                    unitToughness.Content = "4";
+                    unitWounds.Content = "5";
+                    unitSave.Content = "3";
+                    unitLeadership.Content = "6";
+                    unitObjectiveControl.Content = "1";
+                    unitInvulnerableSave.Content = "4";
+                    unitFeelNoPain.Content = "-";
+                    break;
+                case "Apothecary":
+                    unitMovement.Content = "6";
+                    unitToughness.Content = "4";
+                    unitWounds.Content = "4";
+                    unitSave.Content = "3";
+                    unitLeadership.Content = "6";
+                    unitObjectiveControl.Content = "1";
+                    unitInvulnerableSave.Content = "-";
+                    unitFeelNoPain.Content = "-";
+                    break;
+                case "Intercessors":
+                    unitMovement.Content = "6";
+                    unitToughness.Content = "4";
+                    unitWounds.Content = "2";
+                    unitSave.Content = "3";
+                    unitLeadership.Content = "6";
+                    unitObjectiveControl.Content = "2";
+                    unitInvulnerableSave.Content = "-";
+                    unitFeelNoPain.Content = "-";
+                    break;
+                case "Bladeguard":
+                    unitMovement.Content = "6";
+                    unitToughness.Content = "4";
+                    unitWounds.Content = "3";
+                    unitSave.Content = "3";
+                    unitLeadership.Content = "6";
+                    unitObjectiveControl.Content = "1";
+                    unitInvulnerableSave.Content = "4";
+                    unitFeelNoPain.Content = "-";
+                    break;
+                case "Gladiator Lancer":
+                    unitMovement.Content = "10";
+                    unitToughness.Content = "10";
+                    unitWounds.Content = "12";
+                    unitSave.Content = "3";
+                    unitLeadership.Content = "6";
+                    unitObjectiveControl.Content = "3";
+                    unitInvulnerableSave.Content = "-";
+                    unitFeelNoPain.Content = "-";
+                    break;
+            }
+            unitMovement.Refresh();
+            unitToughness.Refresh();
+            unitWounds.Refresh();
+            unitSave.Refresh();
+            unitLeadership.Refresh();
+            unitObjectiveControl.Refresh();
+            unitInvulnerableSave.Refresh();
+            unitFeelNoPain.Refresh();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -64,6 +170,20 @@ namespace WH40K_GUI_UAT_MS539_ML
         {
             RandomNumber rn = new RandomNumber(1, 150);
             randomizerLbl.Content = rn.getRandomNumber().ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private void UnitList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updateUnitWargear(UnitList.SelectedItem.ToString());
+            updateUnitStats(UnitList.SelectedItem.ToString());
         }
     }
 }
